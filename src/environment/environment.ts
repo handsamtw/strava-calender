d5fdecdf4835afe3c99384cbdb22369a97c8bc3b
Shousan Liao <samliao980822@gmail.com>
2024-01-23 16:35:38 -0500

    change backend configuration

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const DevEnvironment = {
   production: false,
-  BACKEND_ENDPOINT: 'http://127.0.0.1:5000',
+  BACKEND_ENDPOINT: 'http://127.0.0.1:8000',
   REDIRECT_URI_AFTER_AUTH: 'http://127.0.0.1:4200/loading',
 };
